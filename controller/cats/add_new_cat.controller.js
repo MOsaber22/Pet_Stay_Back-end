b57a396a66edcf083dd80f14bde44c09d25b23ec
M0Saber <eng.mo.saber11@gmial.com>
2026-07-08 22:23:03 +0300

    adding 'add new cat' controller

--- controller/cats/add_new_cat.controller.js
+++ controller/cats/add_new_cat.controller.js
@@ -1,6 +1,30 @@
+const catsModel = require("../../model/cats.model");
 
-const addNewCat = (req, res) => {
-    console.log("addNewCat controller called");
+const addNewCat = async (req, res) => {
+    const {name, age, gender, breed, image} = req.body;
+    if (!name || !age || !gender || !breed || !image) {
+        return res.status(400).json({
+            status_code: 400,
+            message: "please enter all required fields",
+            data: null,
+        })
+    }
+    try {
+        const newCat = await catsModel.create(req.body);
+        res.status(201).json({
+            status_code: 201,
+            message: "New cat added successfully",
+            data: newCat,
+        });
+    }
+    catch (err) {
+        res.status(500).json({
+            status_code: 500,
+            message: err.message,
+            data: null,
+        })
+    }
+
 };
 
 module.exports = addNewCat;
