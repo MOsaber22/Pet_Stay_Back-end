3f0748f4242803c6d08c069c0a20ec661977054c
MariooX20 <ATTack6599@gmail.com>
2026-07-11 23:01:35 +0300

    Remove delete user function

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,13 +5,13 @@ const createUser = require("../controller/user/create_user.controller");
 const getAllUsers = require("../controller/user/get_all_users.controller");
 const getUserById = require("../controller/user/get_user_by_id.controller");
 const updateUser = require("../controller/user/update_user.controller");
-const deleteUser = require("../controller/user/delete_user.controller");
+
 const isAdmin = require("../middleware/isAdmin.middleware");
 
 routes.post("/", createUser);
 routes.get("/", isAdmin, getAllUsers);
 routes.get("/:userId", getUserById);
 routes.put("/:userId", isAdmin, updateUser);
-routes.delete("/:userId", isAdmin, deleteUser);
+
 
 module.exports = routes;
