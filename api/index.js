660d28c52250fb77826fa76c45697cfe2164ae12
M0Saber <eng.mo.saber11@gmial.com>
2026-07-13 12:20:45 +0300

    update cors

--- api/index.js
+++ api/index.js
@@ -14,8 +14,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 const DB_URL = process.env.DB_URL;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 const allowedOrigins = [
   "https://pet-stay-back-end.vercel.app/",
@@ -34,6 +32,9 @@ app.use(
     credentials: true,
   }),
 );
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1/cart", cartRoutes);
 app.use("/api/v1/cats", catsRoutes);
