759a35a6a8addf975fe0a667f3b5a80c8bf65528
Mina Raafat <minaraafat760@gmail.com>
2026-07-13 14:26:02 +0300

    fix backend url

--- api/index.js
+++ api/index.js
@@ -59,8 +59,8 @@ app.get("/", (req, res) => {
   });
 });
 
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
 module.exports = app;
